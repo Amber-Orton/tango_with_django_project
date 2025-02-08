9b606208a94a85671b129cb6f83fd441aa5dafd9
“Amber <2909415o@student.gla.ac.uk>
2025-02-08 17:04:29 +0000

    5.5

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#used for dynami assignment of templates file path on differnt machines
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+#direction to the static file
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+#direction to media file
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -25,12 +33,8 @@ SECRET_KEY = 'yr38pes&jf!ix5d3zntq0p%oobcqhg610i6!bu459xzy-mqznq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#used for dynami assignment of templates file path on differnt machines
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
-#direction to the static file
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 
 
@@ -72,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', 
             ],
         },
     },
